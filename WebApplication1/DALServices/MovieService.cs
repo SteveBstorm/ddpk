ba7a24fe8d234fa92db7d750f573ff7b12fe92b7
steve <steve.lorent@bstorm.be>
2024-06-23 21:33:23 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication1/DALServices/MovieService.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using WebApplication1.Models;
+
+namespace WebApplication1.DALServices
+{
+    public class MovieService
+    {
+        private SqlConnection _connection;
+
+        public MovieService(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public List<Movie> GetAll()
+        {
+            string sql = "SELECT * FROM Movie";
+
+            return _connection.Query<Movie>(sql).ToList();
+        }
+
+        public Movie Get(int id)
+        {
+            string sql = "SELECT * FROM Movie WHERE Id = @id";
+            return _connection.QueryFirst<Movie>(sql, new {id});
+        }
+
+        public void Create(Movie movie)
+        {
+            string sql = "INSERT INTO Movie (Titre, Description, Real) " +
+                "VALUES (@Titre, @Description, @Real)";
+
+            _connection.Execute(sql, movie);
+        }
+    }
+}
---
+++ WebApplication1/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        public string Real { get; set; }
+    }
+}
